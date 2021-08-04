cfd92b1b8357bb0996d719f8ff6da174e924c5db
Sanjeewa Fernando <sanjeewaf@99x.io>
2021-08-04 09:04:42 +0530

    button switch completed

--- controller.js
+++ controller.js
@@ -1,12 +1,27 @@
-const { Board, Led } = require("johnny-five");
+const { Board, Led, Button } = require("johnny-five");
 const board = new Board({ port: "COM5" });
 
-board.on("ready", () => {
+board.on("ready", function () {
+  const button = new Button({
+    board: board,
+    pin: 8,
+    holdtime: 1000,
+    invert: false,
+  });
+
   const led = new Led(13);
 
-  led.blink(1000);
+  board.repl.inject({
+    button: button,
+  });
 
-  board.on("exit", () => {
+  button.on("down", function () {
+    console.log("down");
+    led.on();
+  });
+
+  button.on("up", function () {
+    console.log("up");
     led.off();
   });
 });
